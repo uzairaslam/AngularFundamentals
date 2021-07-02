0ac03ae34a0dd78e0c94dccec85156ca17b1f6b0
Uzair Aslam <uzairaslam64@gmail.com>
2021-07-02 22:25:04 +0500

    Implemented Input, Output and OnChanges

---
+++ app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnChanges, Input, Output, EventEmitter } from "@angular/core";
+import { Passenger } from "../../models/passenger.interface";
+
+@Component({
+  selector: 'passenger-detail',
+  styleUrls: ['passenger-detail.component.scss'],
+  template: `
+    <div>
+      <span class="status" [class.checked-In]="detail.checkedIn"></span>
+        <div *ngIf="editing">
+          <input type="text" [value]="detail.fullname" (input)="onNameChange(name.value)" #name>
+        </div>
+        <div *ngIf="!editing">
+          {{ detail.fullname }}
+        </div>
+        <div class="date">Check in date: {{ detail.checkedInDate ? (detail.checkedInDate | date: 'y,MMMM dd') : null}} </div>
+        <div class="children">Children: {{ detail.children?.length || 0  }}</div>
+        <div>
+          <button (click)="toggleEdit()">{{ editing ? 'Done' : 'Edit' }}</button>
+          <button (click)="onRemove()">Remove</button>
+        </div>
+    </div>
+  `
+})
+export class PassengerDetailComponent implements OnChanges {
+  editing: boolean = false;
+
+  @Output()
+  edit: EventEmitter<any> = new EventEmitter();
+
+  @Output()
+  remove: EventEmitter<any> = new EventEmitter();
+
+  @Input()
+  detail: Passenger;
+
+  constructor() {
+
+  }
+
+  ngOnChanges(changes) {
+    if(changes.detail)
+      this.detail = Object.assign({}, changes.detail.currentValue);
+  }
+
+  onNameChange(name: string) {
+    this.detail.fullname = name;
+  }
+
+  toggleEdit() {
+    if(this.editing)
+      this.edit.emit(this.detail);
+
+    this.editing = !this.editing;
+  }
+
+  onRemove() {
+    this.remove.emit(this.detail);
+  }
+}
--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Passenger } from "../../models/passenger.interface";
 
 @Component({
@@ -6,42 +6,58 @@ import { Passenger } from "../../models/passenger.interface";
   styleUrls: ['passenger-dashboard.component.scss'],
   template: `
     <div>
-      <h3>Airline Passengers</h3>
-      <ul>
-        <li *ngFor="let passenger of passengers; let i=index">
-        <span class="status" [class.checked-In]="passenger.checkedIn"></span>
-        {{ i }}: {{ passenger.fullname }}
-        <div class="date">Check in date: {{ passenger.checkedInDate ? (passenger.checkedInDate | date: 'y,MMMM dd') : null}} </div>
-        <div class="children">Children: {{ passenger.children?.length || 0  }}</div>
-        </li>
-      </ul>
+      <passenger-count [items]="passengers"></passenger-count>
+      <div *ngFor="let passenger of passengers"> {{ passenger.fullname }}</div>
+      <passenger-detail *ngFor="let passenger of passengers"
+      [detail]="passenger"
+      (remove)="handleRemove($event)"
+      (edit)="handleEdit($event)">
+      </passenger-detail>
     </div>
   `
 })
-export class PassengerDashboardComponent {
-passengers: Passenger[] = [{
-  id: 1,
-  fullname: 'M. Uzair Aslam',
-  checkedIn: true,
-  checkedInDate: 1490742000000,
-  children: null
-},{
-  id: 2,
-  fullname: 'M. Zunair Aslam',
-  checkedIn: false,
-  checkedInDate: null,
-  children: [{ name: 'FChild', age: 5}, { name: 'SChild', age: 3}]
-},{
-  id: 3,
-  fullname: 'Bilal Afzal',
-  checkedIn: true,
-  checkedInDate: 1490742230000,
-  children: [{ name: 'Fchile', age: 10}]
-},{
-  id: 4,
-  fullname: 'Jawad Ali Maken',
-  checkedIn: false,
-  checkedInDate: null,
-  children: null
-}]
+export class PassengerDashboardComponent implements OnInit {
+  passengers: Passenger[];
+  constructor() {
+
+  }
+ngOnInit(): void {
+  console.log('passengers initialized');
+  this.passengers = [{
+    id: 1,
+    fullname: 'M. Uzair Aslam',
+    checkedIn: true,
+    checkedInDate: 1490742000000,
+    children: null
+  },{
+    id: 2,
+    fullname: 'M. Zunair Aslam',
+    checkedIn: false,
+    checkedInDate: null,
+    children: [{ name: 'FChild', age: 5}, { name: 'SChild', age: 3}]
+  },{
+    id: 3,
+    fullname: 'Bilal Afzal',
+    checkedIn: true,
+    checkedInDate: 1490742230000,
+    children: [{ name: 'Fchile', age: 10}]
+  },{
+    id: 4,
+    fullname: 'Jawad Ali Maken',
+    checkedIn: false,
+    checkedInDate: null,
+    children: null
+  }]
+}
+handleEdit(event: Passenger) {
+  this.passengers = this.passengers.map(passenger => {
+    if(passenger.id === event.id)
+      passenger = Object.assign({}, passenger, event);
+    return passenger;
+  });
+}
+
+handleRemove(event: Passenger) {
+  this.passengers = this.passengers.filter(passenger => passenger.id != event.id);
+}
 }
